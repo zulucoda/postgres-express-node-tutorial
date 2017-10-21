d990ab314a3a96a0c6ee9fe764a96e05ede91956
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2017-10-21 22:15:58 +0200

    adding nodemon and www to start express app on port 8000 :bowtie:

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//log request to console
+app.use(logger('dev'));
+
+//parse incoming requests
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//setup default catch-all route
+app.get('*', (req, res) => res.status(200).send({
+  message: 'zulucoda express up and running!!!'
+}));
+
+module.exports = app;
